bf66e8814e7b202527af5cfb9f93b9e67c05dbb1
j0yu <josephyu2712+github@gmail.com>
2017-08-02 14:30:01 +0100

    [settings] Changed settings panel form for local attach

--- src/ca/rightsomegoodgames/mayacharm/settings/MCSettingsPanel.java
+++ src/ca/rightsomegoodgames/mayacharm/settings/MCSettingsPanel.java
@@ -1,16 +1,23 @@
 package ca.rightsomegoodgames.mayacharm.settings;
 
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.text.StringUtil;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class MCSettingsPanel {
     private JPanel myPanel;
     private JTextField hostField;
     private JTextField portField;
     private JTextArea mayaString;
+    private JTextArea pydevdString;
+    private JButton resetPortButton;
+    private JButton resetHostButton;
+    private JButton newSocketButton;
 
     private final String mayaDefaultString;
     private final  MCSettingsProvider mcSettingsProvider;
@@ -35,6 +42,19 @@ public class MCSettingsPanel {
                 mayaString.setText(String.format(mayaDefaultString, portField.getText(), portField.getText()));
             }
         });
+
+//        Wire up the button to generate new port
+//        resetPortButton.addActionListener(actionEvent -> {
+//            if (actionEvent.getSource() == resetPortButton) {
+//                try {
+//                    setAttachSocket(generateNewAttachSocket());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    Messages.showErrorDialog(String.format("Failed to find free socket port, defaulting to %d instead", MCSettingsProvider.DEFAULT_PORT),
+//                                             "No Free Ports");
+//                }
+//            }
+//        });
         reset();
     }
 
@@ -51,8 +71,14 @@ public class MCSettingsPanel {
         return StringUtil.parseInt(portField.getText(), -1);
     }
 
-    public void setPythonCommandPort(Integer value) {
-        portField.setText(value.toString());
+    public void setPythonCommandPort(Integer value) { portField.setText(value.toString()); }
+
+    public ServerSocket generateNewAttachSocket() throws IOException{
+        return new ServerSocket(0);
+    }
+
+    public void setAttachSocket(ServerSocket newSocket) {
+
     }
 
     public String getPythonHost() {
